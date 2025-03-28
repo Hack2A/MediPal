60f04f9cd0018baf287e0b373b4e456e2432dc80
adios-xo <adityaontech@gmail.com>
2025-03-28 18:54:25 +0530

    Refactor login and registration routes for improved layout and consistency

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -32,8 +32,12 @@ const AppRoutes = () => {
                     } />
                     <Route path="/login" element={
                         <>
+                        <div className="flex flex-col min-h-screen">
                             <Navbar />
+                            <div className="flex-grow flex items-center justify-center bg-gray-100">
                             <Login />
+                            </div>
+                        </div>
                         </>
                     } />
                     <Route path="/register" element={
@@ -44,14 +48,22 @@ const AppRoutes = () => {
                     } />
                     <Route path="/registeruser" element={
                         <>
+                        <div className="flex flex-col min-h-screen">
                             <Navbar />
+                            <div className="flex-grow flex items-center justify-center bg-gray-100">
                             <UserRegister />
+                            </div>
+                        </div>
                         </>
                     } />
                     <Route path="/registerdoctor" element={
                         <>
+                        <div className="flex flex-col min-h-screen">
                             <Navbar />
+                            <div className="flex-grow flex items-center justify-center bg-gray-100">
                             <DocRegister />
+                            </div>
+                        </div>
                         </>
                     } />
                     <Route path="/about" element={
