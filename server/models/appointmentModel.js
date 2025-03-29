1239accc4e954067dab03a63323c704445a5c3e5
im-satyam <157481814+im-satyam@users.noreply.github.com>
2025-03-29 09:52:07 +0530

    Adding docname

--- server/models/appointmentModel.js
+++ server/models/appointmentModel.js
@@ -8,6 +8,7 @@ const appointmentSchema = new mongoose.Schema({
   },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   userName: { type: String, required: true },
+  docName: { type: String, required: true },
   userEmail: { type: String, required: true },
   appointmentDate: { type: Date, required: true },
   timeSlot: { type: String, required: true }, // Example: "10:00 AM - 11:00 AM"
