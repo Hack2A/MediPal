89f1ca471226fea8cb0ce7a1f0d0d0bdd9169746
Satyam Kumar <157481814+im-satyam@users.noreply.github.com>
2025-07-21 00:14:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://akshatp17.github.io",
+  credentials: true
+}));
 app.use(bodyParser.json());
 connectDB();
 app.use("/v1", require("./router/authRouter"));
